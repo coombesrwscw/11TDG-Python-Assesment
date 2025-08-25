3d98d906bd39993bd5c32e22498cf8cd5f633792
coombesrwscw <159972148+coombesrwscw@users.noreply.github.com>
2025-08-25 12:00:56 +1200

    got the fight function to work properly

--- resubmit.py
+++ resubmit.py
@@ -4,7 +4,7 @@ inventory=[]
 equiplist=[]
 itemlist=["sword", "shield", "potion", "boot"]
 # enemies
-enemies=["goblin"=={"name"=="Goblin", "atk"==1, "blk"==0, "hel"==5, "dmg"==1}, "orc"=={"name"=="Orc", "atk"==2, "blk"==1, "hel"==10, "dmg"==2},]
+enemies=[{"name": "Goblin", "atk": 1, "blk": 0, "hel": 5, "dmg": 1}, {"name": "Orc", "atk": 2, "blk": 1, "hel": 10, "dmg": 2},]
 goblin={"name": "Goblin", "atk": 1, "blk": 0, "hel": 5, "dmg": 1}
 
 import random
@@ -23,36 +23,39 @@ def applyequips():
 def invalid_input():
     input("Invalid input. Please reinput your choice.")
     return
+
+def fight():
+    global enemy
+    input("Something appears in front of you!")
+    enemy=random.choices(enemies, weights=[10,6], k=1)[0]
+    input("it's a {}!".format(enemy["name"]))
+    while enemy["hel"] > 0:
+        playerturn()
+
 def playerturn():
     playerturn=input("Your turn! Will you attack/block/escape?")
     if playerturn == "attack":
-            if random.randint(1, 3)+(enemies[goblin["blk"]]) <= atk:
-                input("You hit the goblin for {} damage!".format(dmg))
-                goblin["hel"] -= dmg
-                if enemies[goblin["hel"]] <= 0:
-                    input("The goblin has been defeated!")
+            # note to self: fix dmg display
+            if random.randint(-1, 1)+(enemy["blk"]) <= atk:
+                input("You hit the {} for {} damage!".format(enemy["name"], dmg))
+                enemy["hel"] -= dmg+(0, 1)
+                if enemy["hel"] <= 0:
+                    input("The {} has been defeated!".format(enemy["name"]))
                 else:
-                    input("The goblin has {} health remaining.".format(enemies[goblin["hel"]]))   
+                    input("The {} has {} health remaining.".format(enemy["name"], enemy["hel"]))   
             else:
                 input("You missed!")
     elif playerturn == "block":
             blk+=1
-            input("You brace for the goblin's attack, increasing your block chance!")
+            input("You brace for the {}'s attack, increasing your block chance!".format(enemy["name"]))
     elif playerturn == "escape":
-            if random.randint(1, 3)+(goblin["agi"]) <= agi:
-                input("You successfully escaped the goblin!")
+            if random.randint(-1, 1)+(enemy["agi"]) <= agi:
+                input("You successfully escaped the {}!".format(enemy["name"]))
                 return
             else:
                 input("You failed to escape!")
 
-def fight():
-    input("Something appears in front of you!")
-    enemy=random.choices(enemies, weights=[10,6], k=1)[0]
-    input("it's a {}!".format(enemy))
-    if enemy == "goblin":
-        goblinfight()
-
-#stats (atk=attack success chance, blk=block success chance, agi=escape success chance, hp=health points, dmg=damage)
+# player stats (atk=attack success chance, blk=block success chance, agi=escape success chance, hp=health points, dmg=damage)
 atk=1
 blk=1
 agi=1
